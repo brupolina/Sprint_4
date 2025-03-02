609f57f01e4108402451059020aa883b137e9880
Polina Brudanina <brupolinka@gmail.com>
2025-03-02 20:22:59 +0300

    Commit_updated tests

--- src/main/java/ru/yandex/practikum/HomePageScooter.java
+++ src/main/java/ru/yandex/practikum/HomePageScooter.java
@@ -14,42 +14,42 @@ public class HomePageScooter {
 
     // Вопросы о важном
     // Сколько это стоит? И как оплатить?
-    private By question1 = By.xpath(".//div[@class='accordion__item'][1]");
+    private By costQuestion = By.xpath(".//div[@class='accordion__item']");
     // Хочу сразу несколько самокатов! Так можно?
-    private By question2 = By.xpath(".//div[@class='accordion__item'][2]");
+    private By multipleScootersQuestion = By.xpath(".//div[@class='accordion__item']");
     // Как рассчитывается время аренды?
-    private By question3 = By.xpath(".//div[@class='accordion__item'][3]");
+    private By rentalPeriodQuestion = By.xpath(".//div[@class='accordion__item']");
     // Можно ли заказать самокат прямо на сегодня?
-    private By question4 = By.xpath(".//div[@class='accordion__item'][4]");
+    private By todayRentalQuestion = By.xpath(".//div[@class='accordion__item']");
     // Можно ли продлить заказ или вернуть самокат раньше?
-    private By question5 = By.xpath(".//div[@class='accordion__item'][5]");
+    private By modifyRentalQuestion = By.xpath(".//div[@class='accordion__item']");
     // Вы привозите зарядку вместе с самокатом?
-    private By question6 = By.xpath(".//div[@class='accordion__item'][6]");
+    private By includeChargerQuestion = By.xpath(".//div[@class='accordion__item']");
     // Можно ли отменить заказ?
-    private By question7 = By.xpath(".//div[@class='accordion__item'][7]");
+    private By orderCancellationQuestion = By.xpath(".//div[@class='accordion__item']");
     // Я живу за МКАДом, привезёте?
-    private By question8 = By.xpath(".//div[@class='accordion__item'][8]");
+    private By deliveryBeyondMkadQuestion = By.xpath(".//div[@class='accordion__item']");
 
     // Ответы на вопросы о важном
     // Сутки — 400 рублей. Оплата курьеру — наличными или картой.
-    private By answer1 = By.id("accordion__panel-0");
+    private By costAnswer = By.id("accordion__panel-0");
     // Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.
-    private By answer2 = By.id("accordion__panel-1");
+    private By multipleScootersAnswer = By.id("accordion__panel-1");
     // Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.
-    private By answer3 = By.id("accordion__panel-2");
+    private By rentalPeriodAnswer = By.id("accordion__panel-2");
     // Только начиная с завтрашнего дня. Но скоро станем расторопнее.
-    private By answer4 = By.id("accordion__panel-3");
+    private By todayRentalAnswer = By.id("accordion__panel-3");
     // Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.
-    private By answer5 = By.id("accordion__panel-4");
+    private By modifyRentalAnswer = By.id("accordion__panel-4");
     // Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.
-    private By answer6 = By.id("accordion__panel-5");
+    private By includeChargerAnswer = By.id("accordion__panel-5");
     // Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.
-    private By answer7 = By.id("accordion__panel-6");
+    private By orderCancellationAnswer = By.id("accordion__panel-6");
     // Да, обязательно. Всем самокатов! И Москве, и Московской области.
-    private By answer8 = By.id("accordion__panel-7");
+    private By deliveryBeyondMkadAnswer = By.id("accordion__panel-7");
 
     // Верхняя кнопка Заказать
-    private By UpOrderButton = By.xpath(".//button[text()='Заказать'][1]");
+    private By UpOrderButton = By.xpath(".//button[text()='Заказать']");
     // Нижняя кнопка Заказать
     private By DownOrderButton = By.xpath(".//div[contains(@class, 'Home_FinishButton')]/button");
 
@@ -58,31 +58,24 @@ public class HomePageScooter {
     }
 
     // Открыть вопросы о важном
-    public void clickQuestion1() {
-        driver.findElement(question1).click();
+    public void clickCostQuestion() { driver.findElement(costQuestion).click(); }
+    public void clickMultipleScootersQuestion() {
+        driver.findElement(multipleScootersQuestion).click();
     }
-    public void clickQuestion2() {
-        driver.findElement(question2).click();
+    public void clickRentalPeriodQuestion() {
+        driver.findElement(rentalPeriodQuestion).click();
     }
-    public void clickQuestion3() {
-        driver.findElement(question3).click();
+    public void clickTodayRentalQuestion() {
+        driver.findElement(todayRentalQuestion).click();
     }
-    public void clickQuestion4() {
-        driver.findElement(question4).click();
+    public void clickModifyRentalQuestion() {
+        driver.findElement(modifyRentalQuestion).click();
     }
-    public void clickQuestion5() {
-        driver.findElement(question5).click();
+    public void clickIncludeChargerQuestion() {
+        driver.findElement(includeChargerQuestion).click();
     }
-    public void clickQuestion6() {
-        driver.findElement(question6).click();
-    }
-    public void clickQuestion7() {
-        driver.findElement(question7).click();
-    }
-    public void clickQuestion8() {
-        driver.findElement(question8).click();
-
-    }
+    public void clickOrderCancellationQuestion() { driver.findElement(orderCancellationQuestion).click(); }
+    public void clickDeliveryBeyondMkadQuestion() { driver.findElement(deliveryBeyondMkadQuestion).click(); }
 
     // Сравнить ответы на вопросы о важном с ожидаемым ответом
     public void isCorrectText(String answer, String text) {
@@ -90,29 +83,29 @@ public class HomePageScooter {
     }
 
     // Получить текст ответов
-    public String getAnswer1() {
-        return driver.findElement(answer1).getText();
+    public String getCostAnswer() {
+        return driver.findElement(costAnswer).getText();
     }
-    public String getAnswer2() {
-        return driver.findElement(answer2).getText();
+    public String getMultipleScootersAnswer() {
+        return driver.findElement(multipleScootersAnswer).getText();
     }
-    public String getAnswer3() {
-        return driver.findElement(answer3).getText();
+    public String getRentalPeriodAnswer() {
+        return driver.findElement(rentalPeriodAnswer).getText();
     }
-    public String getAnswer4() {
-        return driver.findElement(answer4).getText();
+    public String getTodayRentalAnswer() {
+        return driver.findElement(todayRentalAnswer).getText();
     }
-    public String getAnswer5() {
-        return driver.findElement(answer5).getText();
+    public String getModifyRentalAnswer() {
+        return driver.findElement(modifyRentalAnswer).getText();
     }
-    public String getAnswer6() {
-        return driver.findElement(answer6).getText();
+    public String getIncludeChargerAnswer() {
+        return driver.findElement(includeChargerAnswer).getText();
     }
-    public String getAnswer7() {
-        return driver.findElement(answer7).getText();
+    public String getOrderCancellationAnswer() {
+        return driver.findElement(orderCancellationAnswer).getText();
     }
-    public String getAnswer8() {
-        return driver.findElement(answer8).getText();
+    public String getDeliveryBeyondMkadAnswer() {
+        return driver.findElement(deliveryBeyondMkadAnswer).getText();
     }
 
     // Кликнуть по кнопкам Заказать
---
+++ src/test/java/ru/yandex/practikum/scooterTest/FQATests.java
@@ -0,0 +1,198 @@
+package ru.yandex.practikum.scooterTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static ru.yandex.practikum.Resources.*;
+
+@RunWith(Parameterized.class)
+public class FQATests {
+
+    private WebDriver driver;
+    private HomePageScooter objHomePage;
+
+    // Параметр для передачи в тест
+    private final By questionMethod;
+    private final String answerMethod;
+    private final String expectedText;
+
+    // Конструктор для получения параметров
+    public FQATests(By questionMethod, String answerMethod, String expectedText) {
+        this.questionMethod = questionMethod;
+        this.answerMethod = answerMethod;
+        this.expectedText = expectedText;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"clickCostQuestion", "getCostAnswer", COST_ANSWER},
+                {"clickMultipleScootersQuestion", "getMultipleScootersAnswer", MULTIPLE_SCOOTERS_ANSWER},
+                {"clickRentalPeriodQuestion", "getRentalPeriodAnswer", RENTAL_PERIOD_ANSWER},
+                {"clickTodayRentalQuestion", "getTodayRentalAnswer", TODAY_RENTAL_ANSWER},
+                {"clickModifyRentalQuestion", "getModifyRentalAnswer", MODIFY_RENTAL_ANSWER},
+                {"clickIncludeChargerQuestion", "getIncludeChargerAnswer", INCLUDE_CHARGER_ANSWER},
+                {"clickOrderCancellationQuestion", "getOrderCancellationAnswer", ORDER_CANCELLATION_ANSWER},
+                {"clickDeliveryBeyondMkadQuestion", "getDeliveryBeyondMkadAnswer", DELIVERY_BEYOND_MKAD_ANSWER}
+        });
+    }
+
+    @Before
+    public void startUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://qa-scooter.praktikum-services.ru");
+
+        // Перемещение к секции FAQ
+        scrollToFAQSection();
+
+        objHomePage = new HomePageScooter(driver);
+    }
+
+    private void scrollToFAQSection() {
+        ((JavascriptExecutor)driver).executeScript(
+                "document.querySelector('.accordion').scrollIntoView();"
+        );
+    }
+
+    @Test
+    public void QACorrectAnswerTextTest() {
+        try {
+            // Шаг 1: Кликаем по вопросу
+            objHomePage.clickQuestion(questionMethod);
+
+            // Шаг 2: Получаем текст ответа
+            String actualText = objHomePage.getAnswerText(By.id(answerMethod));
+
+            // Проверка: сравниваем ожидаемый и фактический текст
+            if (!actualText.equals(expectedText)) {
+                throw new AssertionError("Текст ответа не совпадает с ожидаемым\n" +
+                        "Ожидаемый: " + expectedText + "\n" +
+                        "Фактический: " + actualText);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка при выполнении теста: " + e.getMessage());
+        }
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
+// Page Object класс
+class HomePageScooter {
+    private WebDriver driver;
+
+    // Локаторы вопросов
+    private final By costQuestion = By.xpath("//div[contains(.,'Сколько стоит прокат')]");
+    private final By multipleScootersQuestion = By.xpath("//div[contains(.,'Можно ли взять несколько самокатов')]");
+    private final By rentalPeriodQuestion = By.xpath("//div[contains(.,'На какой срок можно взять')]");
+    private final By todayRentalQuestion = By.xpath("//div[contains(.,'Можно ли оформить прокат сегодня')]");
+    private final By modifyRentalQuestion = By.xpath("//div[contains(.,'Можно ли изменить срок проката')]");
+    private final By includeChargerQuestion = By.xpath("//div[contains(.,'В стоимость аренды входит зарядка')]");
+    private final By orderCancellationQuestion = By.xpath("//div[contains(.,'Как отменить заказ')]");
+    private final By deliveryBeyondMkadQuestion = By.xpath("//div[contains(.,'Доставляют ли самокаты за МКАД')]");
+
+    // Локаторы ответов
+    private final By costAnswer = By.xpath("//div[contains(.,'Стоимость проката зависит')]");
+    private final By multipleScootersAnswer = By.xpath("//div[contains(.,'Да, вы можете взять')]");
+    private final By rentalPeriodAnswer = By.xpath("//div[contains(.,'От суток до месяца')]");
+    private final By todayRentalAnswer = By.xpath("//div[contains(.,'Оформить прокат можно')]");
+    private final By modifyRentalQuestionAnswer = By.xpath("//div[contains(.,'Да, вы можете изменить')]");
+    private final By includeChargerAnswer = By.xpath("//div[contains(.,'Да, базовая зарядка')]");
+    private final By orderCancellationAnswer = By.xpath("//div[contains(.,'Отменить заказ можно')]");
+    private final By deliveryBeyondMkadAnswer = By.xpath("//div[contains(.,'Доставка доступна')]");
+
+    public HomePageScooter(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Методы для клика по вопросам
+    public void clickCostQuestion() {
+        clickQuestion(costQuestion);
+    }
+
+    public void clickMultipleScootersQuestion() {
+        clickQuestion(multipleScootersQuestion);
+    }
+
+    public void clickRentalPeriodQuestion() {
+        clickQuestion(rentalPeriodQuestion);
+    }
+
+    public void clickTodayRentalQuestion() {
+        clickQuestion(todayRentalQuestion);
+    }
+
+    public void clickModifyRentalQuestion() {
+        clickQuestion(modifyRentalQuestion);
+    }
+
+    public void clickIncludeChargerQuestion() {
+        clickQuestion(includeChargerQuestion);
+    }
+
+    public void clickOrderCancellationQuestion() {
+        clickQuestion(orderCancellationQuestion);
+    }
+
+    public void clickDeliveryBeyondMkadQuestion() {
+        clickQuestion(deliveryBeyondMkadQuestion);
+    }
+
+    // Вспомогательный метод для клика
+    void clickQuestion(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    // Методы для получения текста ответов
+    public String getCostAnswer() {
+        return getAnswerText(costAnswer);
+    }
+
+    public String getMultipleScootersAnswer() {
+        return getAnswerText(multipleScootersAnswer);
+    }
+
+    public String getRentalPeriodAnswer() {
+        return getAnswerText(rentalPeriodAnswer);
+    }
+
+    public String getTodayRentalAnswer() {
+        return getAnswerText(todayRentalAnswer);
+    }
+
+    public String getModifyRentalAnswer() {
+        return getAnswerText(modifyRentalQuestionAnswer);
+    }
+
+    public String getIncludeChargerAnswer() {
+        return getAnswerText(includeChargerAnswer);
+    }
+
+    public String getOrderCancellationAnswer() {
+        return getAnswerText(orderCancellationAnswer);
+    }
+
+    public String getDeliveryBeyondMkadAnswer() {
+        return getAnswerText(deliveryBeyondMkadAnswer);
+    }
+
+    // Вспомогательный метод для получения текста
+    String getAnswerText(By locator) {
+        return driver.findElement(locator).getText();
+    }
+}
